237a50054dd36c17849988817d29eda67886e963
Daniel Arteaga <dani8art.da@gmail.com>
2015-06-28 13:04:23 +0200

    Transform date when angular service receive json date

--- src/main/webapp/scripts/components/entities/task/task.service.js
+++ src/main/webapp/scripts/components/entities/task/task.service.js
@@ -8,10 +8,8 @@ angular.module('tasksadminApp')
                 method: 'GET',
                 transformResponse: function (data) {
                     data = angular.fromJson(data);
-                    var EndDateFrom = data.endDate.split("-");
-                    data.endDate = new Date(new Date(EndDateFrom[0], EndDateFrom[1] - 1, EndDateFrom[2]));
-                    var insertDateFrom = data.insertDate.split("-");
-                    data.insertDate = new Date(new Date(insertDateFrom[0], insertDateFrom[1] - 1, insertDateFrom[2]));
+                    data.endDate = new Date(data.endDate);                    
+                    data.insertDate = new Date(data.insertDate);
                     return data;
                 }
             },
