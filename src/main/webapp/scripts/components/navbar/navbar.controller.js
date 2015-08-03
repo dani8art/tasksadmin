160f5044c8746104334c006f964e314d9c72c164
Daniel Arteaga <dani8art.da@gmail.com>
2015-08-03 19:41:35 +0200

    navbar-link to account with default image

--- src/main/webapp/scripts/components/navbar/navbar.controller.js
+++ src/main/webapp/scripts/components/navbar/navbar.controller.js
@@ -4,7 +4,10 @@ angular.module('tasksadminApp')
     .controller('NavbarController', function ($scope, $location, $state, Auth, Principal) {
         $scope.isAuthenticated = Principal.isAuthenticated;
         $scope.$state = $state;
-		
+		Principal.identity().then(function(account){
+			$scope.user = account;
+		});
+        
         $scope.logout = function () {
             Auth.logout();
             $state.go('principal');
