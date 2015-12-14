043f6c17c2d62e5758508ff3c2786ea91c658408
Daniel Arteaga <darteaga@us.es>
2015-12-14 16:01:09 +0100

    All principal's changes completed, minus footer

--- src/main/webapp/scripts/app/principal/principal.controller.js
+++ src/main/webapp/scripts/app/principal/principal.controller.js
@@ -1,15 +1,21 @@
 'use strict';
 
 angular.module('tasksadminApp')
-    .controller('PrincipalController', function ($scope,$rootScope,$state,Principal,Auth, Task, User) {
+    .controller('PrincipalController', function ($scope,$rootScope,$state,$translate,Principal,Auth, Task, User) {
         Principal.identity().then(function(account) {
             $scope.account = account;
             $scope.isAuthenticated = Principal.isAuthenticated;
         }).then(function(){
+        	$scope.success = null;
+            $scope.error = null;
+            $scope.doNotMatch = null;
+            $scope.errorUserExists = null;
+            $scope.registerAccount = {};
+            
         	if($scope.isAuthenticated()){
         		$state.go("home");	        
         	}else{        		
-        		
+        		$scope.isLogin=true;
         		$scope.login = function () {
                     Auth.login({
                         username: $scope.username,
@@ -27,9 +33,52 @@ angular.module('tasksadminApp')
                         $scope.authenticationError = true;
                     });
                 };
+                
+                $scope.register = function () {
+                    if ($scope.registerAccount.password !== $scope.confirmPassword) {
+                        $scope.doNotMatch = 'ERROR';
+                    } else {
+                        $scope.registerAccount.langKey = $translate.use();
+                        $scope.doNotMatch = null;
+                        $scope.error = null;
+                        $scope.errorUserExists = null;
+                        $scope.errorEmailExists = null;
+
+                        Auth.createAccount($scope.registerAccount).then(function () {
+                            $scope.success = 'OK';
+                        }).catch(function (response) {
+                            $scope.success = null;
+                            if (response.status === 400 && response.data === 'login already in use') {
+                                $scope.errorUserExists = 'ERROR';
+                            } else if (response.status === 400 && response.data === 'e-mail address already in use') {
+                                $scope.errorEmailExists = 'ERROR';
+                            } else {
+                                $scope.error = 'ERROR';
+                            }
+                        });
+                    }
+                };
         		
         		$('#carousel-principal').carousel('cycle');
         		
+        		$scope.changeIsLogin = function(){
+        			$scope.isLogin = !$scope.isLogin;
+        			$scope.clearAll(false);
+        		}
+        		
+        		$scope.clearAll = function(changeLogin){
+        			if(changeLogin==null)
+        				changeLogin = true;
+        			$scope.success = null;
+                    $scope.error = null;
+                    $scope.doNotMatch = null;
+                    $scope.errorUserExists = null;
+                    $scope.registerAccount = {};
+                    if(changeLogin)
+                    	$scope.isLogin=true;
+                    $scope.authenticationError = false;
+                    $("input").val("");
+        		}
         	}
         }).catch(function(){
         	$scope.login = function () {
